5cccf3ca182fcd0f591cd34b18839641764e9947
Laura Bitencourt Silveira <laurabitencourtsilveira@gmail.com>
2025-10-20 22:36:58 -0300

    versao com email

---
+++ nodejs/saudacao-mvc/models/saudacaoModel.js
@@ -0,0 +1,18 @@
+module.exports = {
+  gerarMensagemPersonalizada: (nome, idade, apelido ) => {
+    let faixaEtaria;
+    
+
+    if (idade < 12) {
+      faixaEtaria = "criança";
+    } else if (idade < 18) {
+      faixaEtaria = "adolescente";
+    } else {
+      faixaEtaria = "adulto";
+    }
+
+    
+      
+    return `Olá, ${nome} ou ${apelido}! Você é um(a) ${faixaEtaria}`;
+  }
+};
