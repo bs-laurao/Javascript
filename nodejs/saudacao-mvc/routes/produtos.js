9513703bd9a2c5fc23b049c348a6e04514182764
Laura Bitencourt Silveira <laurabitencourtsilveira@gmail.com>
2025-11-03 21:21:17 -0300

    versão cadastrar produtos

--- nodejs/saudacao-mvc/routes/produtos.js
+++ nodejs/saudacao-mvc/routes/produtos.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const produtosController = require("../controllers/produtosController");
+const produtosController = require('../controllers/produtosController');
 
-router.get("/", produtosController.index);
-
-router.get("/cadastrar", produtosController.formulario);
-
-router.post("/cadastrar", produtosController.cadastrar);
+router.get('/', produtosController.produtos);
+router.get('/camisetas', produtosController.camisetas);
+router.get('/cadastrar', produtosController.formCadProd);
+router.post('/cadastrar', produtosController.cadastrar);
 
 module.exports = router;
